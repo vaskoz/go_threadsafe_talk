0405101a6a8f7c940b89bfcf7b4352ee42872e13
Vasko Zdravevski <vasko.zdravevski@sendgrid.com>
2015-03-31 00:01:59 -0600

    add benchmarks for concurrency primitives: channels and mutex

---
+++ concurrency_benchmark_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"sync"
+	"testing"
+)
+
+func BenchmarkUnbufferedChannel(b *testing.B) {
+	c := make(chan int)
+	for i := 0; i < b.N; i++ {
+		go func(val int) { c <- val }(i)
+		<-c
+	}
+}
+
+func BenchmarkBufferedChannel(b *testing.B) {
+	c := make(chan int, 10)
+	for i := 0; i < b.N; i++ {
+		go func(val int) { c <- val }(i)
+		<-c
+	}
+}
+
+func BenchmarkMutex(b *testing.B) {
+	var mutex sync.Mutex
+	for i := 0; i < b.N; i++ {
+		mutex.Lock()
+		mutex.Unlock()
+	}
+}
+
+func Benchmark90R10WMutex(b *testing.B) {
+	var rwMutex sync.RWMutex
+	for i := 0; i < b.N; i++ {
+		rwMutex.RLock()
+		rwMutex.RUnlock()
+		if i%10 == 0 {
+			rwMutex.Lock()
+			rwMutex.Unlock()
+		}
+	}
+}
